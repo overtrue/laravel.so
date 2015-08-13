19c81df3d91cb9e22bf2b62f1c65b141cfffcfb5
overtrue <anzhengchao@gmail.com>
2015-08-14 01:02:37 +0800

    友链新窗口打开

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     <p class="text-muted credit">
     <span><label for="">合作伙伴：</label>
         @foreach(trans('partials.friends_links') as $label => $url)
-            <a href="{{ $url }}">{{$label}}</a>
+            <a href="{{ $url }}" target="_blank" title="{{$label}}">{{$label}}</a>
         @endforeach
     </span>
     <span class="pull-right">{!! trans('partials.credits') !!} | <a href="{{ url('about') }}">{!! trans('partials.about') !!}</a> | {!! trans('partials.footer_links') !!}</span>
