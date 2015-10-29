f07a8c9399067bcaa3bc2716087e27f97f2c4beb
overtrue <anzhengchao@gmail.com>
2015-10-29 23:46:33 +0800

    Update style.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <div id="footer">
   <div class="container">
     <div class="text-muted row credit">
-        <div class="col-md-8 links">
+        <div class="col-md-7 links">
             <label for="">合作伙伴</label>
             @foreach(trans('partials.friends_links') as $label => $url)
                 ・ <a href="{{ $url }}" target="_blank" title="{{$label}}"> {{$label}} </a>
             @endforeach
         </div>
-        <div class="col-md-4 about"> {!! trans('partials.credits') !!} ・ <a href="{{ url('about') }}"> {!! trans('partials.about') !!} </a> ・ {!! trans('partials.footer_links') !!} </div>
+        <div class="col-md-5 about"> {!! trans('partials.credits') !!} ・ <a href="{{ url('about') }}"> {!! trans('partials.about') !!} </a> ・ {!! trans('partials.footer_links') !!} </div>
     </div>
   </div>
 </div>
