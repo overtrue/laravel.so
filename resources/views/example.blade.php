23f72f27ed91518e985da0f5c357e8f41cfd34f4
jcc <changejian@gmail.com>
2016-08-18 14:39:26 +0800

    Vue 基础配置例子

---
+++ resources/views/example.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Example</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+  <div id="app"></div>
+
+  <script src="{{ asset('js/main.js') }}"></script>
+
+</body>
+</html>
